ec0a2c49beb45a9721bf6aafd75f132ba3cf188e
mihaelailiescu <mihaela.iliescu1708@yahoo.com>
2021-03-10 20:16:42 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<center>
+<?php 
+
+echo "Copyright (c) " . $company_name . " " . date("Y") . " - All Rights Reserved"; 
+
+?>
+</center>
---
+++ variables.php
@@ -0,0 +1,6 @@
+<?php 
+
+$company_name = "Guitar World, Inc.";
+$tagline = "Teaching Guitar Since 2001";
+
+?>
